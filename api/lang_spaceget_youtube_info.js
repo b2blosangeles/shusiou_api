58e2db46d19a86bcbfc4c33ca80e2a07a643dfaf
b2blosangeles <b2blosangeles@gmail.com>
2017-06-01 16:31:00 -0700

    Update lang_spaceget_youtube_info.js

--- api/lang_spaceget_youtube_info.js
+++ api/lang_spaceget_youtube_info.js
@@ -1,5 +1,9 @@
 var ytdl = require(env.space_path + '/api/inc/ytdl-core/node_modules/ytdl-core');
 ytdl.getInfo(req.body.video_url, {},  function(err, info){
-  var r = {vid:info.video_id, title:info.title, length_seconds:parseInt(info.length_seconds), thumbnail_url:info.thumbnail_url};
-  res.send(r);
+  if (err) {
+    res.send(err);
+  } else {
+    var r = {vid:info.video_id, title:info.title, length_seconds:parseInt(info.length_seconds), thumbnail_url:info.thumbnail_url};
+    res.send(r);
+  }  
 });
