e9add55b7909663c2a2a36a9ebdaabeed785447b
b2blosangeles <b2blosangeles@gmail.com>
2017-06-04 16:59:55 -0700

    Update shusiou_video_add_queue.js

--- api/shusiou_video_add_queue.js
+++ api/shusiou_video_add_queue.js
@@ -20,35 +20,10 @@ _f['S0'] = function(cbk) {
   }
 };
 
-_f['S1'] = function(cbk) {
-	var vid = CP.data.S0;
-	if (!vid) {
-		cbk(false);
-		CP.exit = 1;
-		return true;
-	}	
-	var folderP = require(env.space_path + '/api/lang_space/inc/folderP/folderP');
-	var fp = new folderP();
-	fp.build(folder_base + vid + '/', function() {
-		cbk(vid);
-	});	
-};
-_f['S2'] = function(cbk) {
-	var vid = CP.data.S0, fn = folder_base + vid + '/matrix.data', v=[];
-        for (var i=0; i < 30; i++) { v[i] = 0; }
-	pkg.fs.writeFile(fn, JSON.stringify(v), function (err) {
-		if (err) 
-		  	cbk(err.message);
-		else  
-		 	cbk(v);
-	}); 
-};	
-
-
 CP.serial(
 	_f,
 	function(data) {
-		res.send({_spent_time:data._spent_time, status:data.status, data:data.results.S2});
+		res.send({_spent_time:data._spent_time, status:data.status, data:data.results});
 	},
 	30000
 );
