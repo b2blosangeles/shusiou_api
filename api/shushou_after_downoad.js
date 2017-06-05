4899e4ab9abae458ecc25217fc78c60ea017f956
b2blosangeles <b2blosangeles@gmail.com>
2017-06-05 16:45:14 -0700

    Update shushou_after_downoad.js

--- api/shushou_after_downoad.js
+++ api/shushou_after_downoad.js
@@ -97,7 +97,7 @@ _f['Q3'] = function(cbk) {
 		var str = 'INSERT INTO videos (`source`, `code`, `created`, `status`, `info`) ' +
 			'values ("youtube", "' + CP.data.Q2 + '", NOW(), 0 , "' +  encodeURIComponent(JSON.stringify(CP.data.Q1.info)) + 
 			'"); ';
-		 str += 'DELETE FROM video_queue WHERE `source` = "youtube" AND  `code` = "' + CP.data.Q2.code + '"; ';
+		 str += 'DELETE FROM video_queue WHERE `source` = "youtube" AND  `code` = "' + CP.data.Q2 + '"; ';
 		
 		cbk(str);
 		return true;
