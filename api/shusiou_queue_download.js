0191d1e220e5949246ed9fa60d10db905267f084
b2blosangeles <b2blosangeles@gmail.com>
2017-06-05 13:48:44 -0700

    Update shusiou_queue_download.js

--- api/shusiou_queue_download.js
+++ api/shusiou_queue_download.js
@@ -83,7 +83,7 @@ _f['Q3'] = function(cbk) {
 		fp.build(folder_base  + vid, function() {
 			ytdl.getInfo(url, function(err) {
 				if (err) {
-					cbk({idx:c_m, status:7});
+					cbk({idx:c_m, status:9});
 					return true;
 				}
 				var video = ytdl(url, {range: {start:start, end:end}, quality:'18'});
@@ -130,36 +130,6 @@ _f['Q4'] = function(cbk) {
 	}); 	
 	
 };
-
-/*
-	if (!sid && sid !==0) {
-		cbk(false);
-		CP.exit = 1;
-	}
-	var vid = CP.data.S0;
-	if (!vid) {
-		cbk(false);
-		CP.exit = 1;
-		return true;
-	}
-	var step = 50000000;
-	var start = sid * step;
-	var end = (sid + 1) * step -1;
-	
- 	var video = ytdl(vurl, {range: {start:start, end:end}, quality:'18'});	
-	video.pipe(pkg.fs.createWriteStream(folder_base + vid + '/' + sid+'.mp4'));
-
-	video.on('data', function(info) {}); 
-
-	video.on('end', function(info) {
-		cbk({sid:sid, value:1});
-	});	
-
-	video.on('error', function() {
-		cbk({sid:sid, value:9});
-	});
-
-*/
 
 CP.serial(
 	_f,
