87dbd9b31c1f77e6cc128be3ec7ef2db2e3b49fe
b2blosangeles <b2blosangeles@gmail.com>
2017-06-05 08:13:26 -0700

    Update shusiou_video_download.js

--- api/shusiou_video_download.js
+++ api/shusiou_video_download.js
@@ -5,31 +5,27 @@ var CP = new pkg.crowdProcess();
 var _f = {};
 
 _f['Q'] = function(cbk) {
-	if (!vid && !vurl) {
-		cbk(false);
-	} else {	
-		var cfg0 = require(env.space_path + '/api/cfg/db.json');
-		var connection = mysql.createConnection(cfg0);
-		connection.connect();
+	var cfg0 = require(env.space_path + '/api/cfg/db.json');
+	var connection = mysql.createConnection(cfg0);
+	connection.connect();
 
-		var str = "SELECT * FROM  `video_queue` WHERE `source` = 'youtube' AND status = 0 ORDERBY created ASC; ";
+	var str = "SELECT * FROM  `video_queue` WHERE `source` = 'youtube' AND `status` = 0 ORDERBY `created` ASC; ";
 
-		connection.query(str, function (error, results, fields) {
-			connection.end();
-			if (error) {
-				cbk(error.message);
-				return true;
+	connection.query(str, function (error, results, fields) {
+		connection.end();
+		if (error) {
+			cbk(error.message);
+			return true;
+		} else {
+			if (results[0]) {
+				cbk(results[0]);
+				CP.skip = true;
 			} else {
-				if (results[0]) {
-					cbk(results[0]);
-					CP.skip = true;
-				} else {
-					cbk(false);
-				}
-
+				cbk(false);
 			}
-		});  
-	}
+
+		}
+	});  
 };
 
 CP.serial(
