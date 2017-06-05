62663564569fc3d95de315855958375465ccd7e7
b2blosangeles <b2blosangeles@gmail.com>
2017-06-05 08:16:18 -0700

    Update shusiou_video_download.js

--- api/shusiou_video_download.js
+++ api/shusiou_video_download.js
@@ -4,7 +4,32 @@ var mysql = require(env.space_path + '/api/inc/mysql/node_modules/mysql');
 var CP = new pkg.crowdProcess();
 var _f = {};
 
-_f['Q'] = function(cbk) {
+_f['Q0'] = function(cbk) {
+	var cfg0 = require(env.space_path + '/api/cfg/db.json');
+	var connection = mysql.createConnection(cfg0);
+	connection.connect();
+
+	var str = 'SELECT * FROM  `video_queue` WHERE `source` = "youtube" AND `status` = 1 ORDERBY `created` ASC; ';
+
+	connection.query(str, function (error, results, fields) {
+		connection.end();
+		if (error) {
+			cbk(error.message);
+			return true;
+		} else {
+			if (results[0]) {
+				cbk(results[0]);
+				CP.skip = true;
+			} else {
+				cbk(false);
+			}
+
+		}
+	});  
+};
+
+
+_f['Q1'] = function(cbk) {
 	var cfg0 = require(env.space_path + '/api/cfg/db.json');
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
