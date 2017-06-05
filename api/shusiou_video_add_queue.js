b1631bf20c2215a5dfaa8ad283172f468ee09258
b2blosangeles <b2blosangeles@gmail.com>
2017-06-05 11:44:13 -0700

    Update shusiou_video_add_queue.js

--- api/shusiou_video_add_queue.js
+++ api/shusiou_video_add_queue.js
@@ -4,7 +4,7 @@ var mysql = require(env.space_path + '/api/inc/mysql/node_modules/mysql');
 var CP = new pkg.crowdProcess();
 var _f = {};
 
-var vurl = req.body.vurl || 'https://www.youtube.com/watch?v=UiH9pPOSFLs';
+var vurl = req.body.vurl || 'https://www.youtube.com/watch?v=UzbRwICWODk';
 var vid = req.body.vid;
 
 _f['Q'] = function(cbk) {
