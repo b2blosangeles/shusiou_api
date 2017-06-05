04ded51608f8dd0fc3c43ac14493b367b4059ad7
b2blosangeles <b2blosangeles@gmail.com>
2017-06-05 08:17:54 -0700

    Update shusiou_video_download.js

--- api/shusiou_video_download.js
+++ api/shusiou_video_download.js
@@ -34,7 +34,7 @@ _f['Q1'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
 
-	var str = 'SELECT * FROM  `video_queue` WHERE `source` = "youtube" AND `status` = 0 ORDERBY `created` ASC; ';
+	var str = 'SELECT * FROM  `video_queue` WHERE `source` = "youtube" AND `status` = 0 ORDER BY `created` ASC; ';
 
 	connection.query(str, function (error, results, fields) {
 		connection.end();
