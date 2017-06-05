3a77fb2091368df6aeaa97e701c14064bbea418e
b2blosangeles <b2blosangeles@gmail.com>
2017-06-04 19:33:24 -0700

    Update shusiou_video_add_queue.js

--- api/shusiou_video_add_queue.js
+++ api/shusiou_video_add_queue.js
@@ -30,7 +30,7 @@ _f['S1'] = function(cbk) {
 		connection.connect();
 
 		connection.query('SHOW TABLES;', function (error, results, fields) {
-			connection.release();
+			connection.end();
 			if (error) {
 				cbk(error.message);
 				return true;
