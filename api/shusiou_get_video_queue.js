17dd26813977784b0adf2962f9ee7df5e989069a
b2blosangeles <b2blosangeles@gmail.com>
2017-06-07 07:31:58 -0700

    Update shusiou_get_video_queue.js

--- api/shusiou_get_video_queue.js
+++ api/shusiou_get_video_queue.js
@@ -25,7 +25,7 @@ _f['Q1'] = function(cbk) {
 CP.serial(
 	_f,
 	function(data) {
-		res.send({_spent_time:data._spent_time, status:data.status, data:data});
+		res.send({_spent_time:data._spent_time, status:data.status, data:data.Q1});
 	},
 	30000
 );
