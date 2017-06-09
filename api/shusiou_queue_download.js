b90a847b93d1a9041c4c1e0f9129838ee77816a3
b2blosangeles <b2blosangeles@gmail.com>
2017-06-08 18:22:13 -0700

    Create shusiou_queue_download.js

--- api/shusiou_queue_download.js
+++ api/shusiou_queue_download.js
@@ -162,17 +162,6 @@ _f['AF1'] = function(cbk) {
 		}
 	});  
 };
-function matrixAfter9(m, idx) {
-	if (idx === 0 || idx === 1 ) return m;
-	var v = [];
-	if (m[idx-1] == 9 && m[idx-2] == 9) {
-		for (var i = 0; i < idx-2; i++) {	
-			v[v.length] = m[i];  
-		}
-		return v;
-	}
-	return m; 
-}
 
 _f['matrix_change'] = function(cbk) {
 	var m = JSON.parse(CP.data.AF1.matrix), v = [];
@@ -196,60 +185,26 @@ _f['matrix_change'] = function(cbk) {
 	connection.query(str, function (error, results, fields) {
 		connection.end();
 		if (error) {
-			cbk(error.message);
+			cbk(false);
 		} else {
 			cbk(v);
 		}
 	}); 	
 	return true;	
 };
-_f['Q0'] = function(cbk) {
-	cbk(1);
-	CP.exit = 1;
-};
 
 _f['AF2'] = function(cbk) {
-
-	if (!CP.data.AF1 || !CP.data.AF1.source_code) {
+	if (!CP.data.AF1 || !CP.data.AF1.source_code || !CP.data.matrix_change) {
 		cbk(false);
 		CP.exit = true;
 	} else {
-		var m = JSON.parse(CP.data.AF1.matrix);	
+		var m = CP.data.matrix_change;	
 		for (var i = 0; i < m.length; i++) {
 			if (m[i] == 0) {
 				cbk(false);
 				return true;
 			}
 		}			
-		for (var i = 0; i < m.length; i++) {
-			if (m[i] == 9) {
-				var v = matrixAfter9(m, i);
-				if (v.length != m.length) {					
-					/--- Save adjusted matrix ---*/
-					var cfg0 = require(env.space_path + '/api/cfg/db.json');
-					var connection = mysql.createConnection(cfg0);
-					connection.connect();
-
-					var str = 'UPDATE `video_queue` SET `matrix` = "' + JSON.stringify(v) + '" '+
-					    'WHERE `source` = "youtube" AND `status` = 0 AND code = "' + CP.data.AF1.code + '"; ';
-
-					connection.query(str, function (error, results, fields) {
-						connection.end();
-						if (error) {
-							cbk(error.message);
-							return true;
-						} else {
-							cbk(false);
-						}
-					}); 						
-					return true;
-				} else {
-					cbk(false);
-					return true;
-				}
-			} 
-		}
-		
 		cbk(CP.data.AF1.code);
 	}
 };
@@ -257,8 +212,6 @@ _f['AF3'] = function(cbk) {
 	if (CP.data.AF2 === false) {
 	 	cbk(false);
 	} else {
-		
-
 		var cfg = require(env.space_path + '/api/cfg/db.json');
 		cfg['multipleStatements'] = true;
 		var connection = mysql.createConnection(cfg);
