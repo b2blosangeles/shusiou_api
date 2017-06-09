6f6a343c29c62c593e90486f6d958ffdd46e6835
b2blosangeles <b2blosangeles@gmail.com>
2017-06-08 19:06:09 -0700

    Create shusiou_delete_video.js

---
+++ api/shusiou_delete_video.js
@@ -0,0 +1,42 @@
+var ytdl = require(env.space_path + '/api/inc/ytdl-core/node_modules/ytdl-core');
+var mysql = require(env.space_path + '/api/inc/mysql/node_modules/mysql');
+
+var folder_base = '/mnt/shusiou-video/youtube/';
+
+var CP = new pkg.crowdProcess();
+var _f = {};
+
+
+_f['AF1'] = function(cbk) {
+	var cfg0 = require(env.space_path + '/api/cfg/db.json');
+	var connection = mysql.createConnection(cfg0);
+	connection.connect();
+
+	var str = 'SELECT * FROM  `video_queue` WHERE `source` = "youtube" AND `status` = 0 ORDER BY `created` ASC LIMIT 1; ';
+
+	connection.query(str, function (error, results, fields) {
+		connection.end();
+		if (error) {
+			cbk(error.message);
+			return true;
+		} else {
+			if (results.length) {
+				cbk(results[0]);
+				CP.skip = true;
+			} else {
+				cbk(false);
+			}
+
+		}
+	});  
+};
+
+
+
+CP.serial(
+	_f,
+	function(data) {
+		res.send({_spent_time:data._spent_time, status:data.status, data:data});
+	},
+	30000
+);
