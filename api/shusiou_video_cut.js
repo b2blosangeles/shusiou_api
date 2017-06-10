3931daee32d4ce7dd5c5809d8dd6f60a50d51891
b2blosangeles <b2blosangeles@gmail.com>
2017-06-10 06:53:38 -0700

    Create shusiou_video_cut.js

--- api/shusiou_video_cut.js
+++ api/shusiou_video_cut.js
@@ -4,7 +4,7 @@ var folder_base = '/mnt/shusiou-video/youtube/';
 
 var s_file = folder_base + 'video/' + video[0],  s =  video[1], l =  video[2];
 
-var fn = folder_base + 'cut/'  + video[0] '/' + s + '_' + l + '.mp4';
+// var fn = folder_base + 'cut/'  + video[0] '/' + s + '_' + l + '.mp4';
 
 var childProcess = require('child_process');
 
@@ -25,51 +25,3 @@ CP.serial(
 	},
 	30000
 );
-return true;
-
-var stream = function() {
-	//res.send(fn+'=s==');
-	//return true;
-	pkg.fs.stat(fn, function(err, data) {
-            if (err) 
-              res.send('it does not exist');
-            else {
-		      var total = data.size;
-		      var range = req.headers.range;
-		      if (range) {
-				var parts = range.replace(/bytes=/, "").split("-");
-				var partialstart = parts[0]; var partialend;
-				  partialend =  parts[1];
-				var start = parseInt(partialstart, 10);
-				var end = partialend ? parseInt(partialend, 10) : total-1;
-				var chunksize = (end-start)+1;
-				var file = pkg.fs.createReadStream(fn, {start:start, end:end});
-			      
-				res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
-					'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
-			       file.pipe(res);
-			} else {
-				res.send('Need streaming player');
-			}
-		}
-	});
-};
-
-pkg.fs.stat(fn, function(err, stat) {
-	// if(err == null) {
-	//	stream();  -async 1 -strict -2
-	// } else {  -vcodec copy -acodec copy
-//	res.send('ffmpeg  -i ' + s_file + ' -ss ' + s + ' -to ' + l + ' -c copy ' + fn +' -y ');
-//	return true;
-	
-		var ls = childProcess.exec('ffmpeg  -i ' + s_file + ' -ss '+ s + ' -t ' + l + ' -c copy ' + fn +' -y ', 		   
-			function (error, stdout, stderr) {
-				stream();
-			});
-			
-/*
-		ls.on('close', (code) => {
-		}); 
-*/		
-	//}
-});
