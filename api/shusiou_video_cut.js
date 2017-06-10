55a4444de18f0e5efc3f84e83960525ef095c82e
b2blosangeles <b2blosangeles@gmail.com>
2017-06-10 07:05:13 -0700

    Create shusiou_video_cut.js

--- api/shusiou_video_cut.js
+++ api/shusiou_video_cut.js
@@ -3,8 +3,9 @@ var video = req.query['video'].split('|'), fn;
 var folder_base = '/mnt/shusiou-video/youtube/';
 
 var s_file = folder_base + 'video/' + video[0],  s =  video[1], l =  video[2];
+var c_folder = folder_base + 'cut/' + video[0] + '/';
 
-// var fn = folder_base + 'cut/'  + video[0] '/' + s + '_' + l + '.mp4';
+var fn = c_folder + s + '_' + l + '.mp4';
 
 var childProcess = require('child_process');
 
@@ -12,7 +13,12 @@ var CP = new pkg.crowdProcess();
 var _f = {};
 
 _f['S0'] = function(cbk) {
-	cbk(true);
+	var folderP = require(env.space_path + '/api/inc/folderP/folderP');
+	var fp = new folderP();
+	fp.build(c_folder, function() {
+		cbk(c_folder);	
+	});
+	
 };
 _f['S1'] = function(cbk) {
 	cbk(true);
