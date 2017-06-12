e1795b5163a9ca45e22107ac93db3bb5d001e0e0
b2blosangeles <b2blosangeles@gmail.com>
2017-06-11 23:02:26 -0700

    Create shusiou_get_video_byid.js

--- api/shusiou_get_video_byid.js
+++ api/shusiou_get_video_byid.js
@@ -21,34 +21,6 @@ _f['Q0'] = function(cbk) {
 	});  
 };
 
-_f['Q1'] = function(cbk) {
-	var cfg0 = require(env.space_path + '/api/cfg/db.json');
-	var connection = mysql.createConnection(cfg0);
-	connection.connect();
-
-	var str = 'SELECT * FROM  `video_queue` WHERE 1 ORDER BY `created` DESC';
-
-	connection.query(str, function (error, results, fields) {
-		connection.end();
-		if (error) {
-			cbk(error.message);
-			return true;
-		} else {
-			for (var i = 0; i < results.length; i++) {
-				var matrix = JSON.parse(results[i].matrix);
-				var matrix_success = 0;
-				for (var j=0; j < matrix.length; j++) {
-					if (parseInt(matrix[j]) == 1 || parseInt(matrix[j]) == 9) matrix_success++;
-				}
-				var rate = Math.round(matrix_success * 1000 / matrix.length)
-				results[i].perc = rate/10;
-			}
-			cbk(results);
-		}
-	});  
-};
-
-
 CP.serial(
 	_f,
 	function(data) {
