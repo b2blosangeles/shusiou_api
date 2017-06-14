656fba5652e543d0ae22b087368fc56435db7acc
b2blosangeles <b2blosangeles@gmail.com>
2017-06-13 22:21:38 -0700

    Create shusiou_get_curriculum.js

--- api/shusiou_get_curriculum.js
+++ api/shusiou_get_curriculum.js
@@ -5,30 +5,25 @@ var _f = {};
 _f['S1'] = function(cbk) {
 	var cfg0 = require(env.space_path + '/api/cfg/db.json');
 	var connection = mysql.createConnection(cfg0);
-	connection.connect();
+	
 
 	var str = 'SELECT * FROM  curriculums WHERE 1; ';
 	
 	connection.query(str, function (error, results, fields) {
-		connection.end();
+		
 		if (error) {
 			cbk(error.message);
 			return true;
 		} else {
-			if (results[0]) {
-				cbk(results);
-				CP.skip = true;
-			} else {
-				cbk(false);
-			}
-
+			cbk(results);
 		}
 	});  
 };
-
+connection.connect();
 CP.serial(
 	_f,
 	function(data) {
+		connection.end();
 		res.send({_spent_time:data._spent_time, status:data.status, data:data});
 	},
 	30000
