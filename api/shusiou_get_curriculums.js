b2c0719eb2fd4542a7b4af337e5c36afc5f0dfb6
b2blosangeles <b2blosangeles@gmail.com>
2017-06-13 22:35:34 -0700

    Create shusiou_get_curriculums.js

--- api/shusiou_get_curriculums.js
+++ api/shusiou_get_curriculums.js
@@ -1,4 +1,7 @@
 var mysql = require(env.space_path + '/api/inc/mysql/node_modules/mysql');
+var cfg0 = require(env.space_path + '/api/cfg/db.json');
+var connection = mysql.createConnection(cfg0);
+
 var CP = new pkg.crowdProcess();
 var _f = {};
 
