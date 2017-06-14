7a358324c18823e782dea752773dcd34cce6efcd
b2blosangeles <b2blosangeles@gmail.com>
2017-06-14 08:24:56 -0700

    Create auth.js

--- api/auth.js
+++ api/auth.js
@@ -198,7 +198,7 @@ switch(req.body.cmd) {
 			_f,
 			function(data) {
 				connection.end();
-				res.send({_spent_time:data._spent_time, status:data.status, data:data.results});
+				res.send({_spent_time:data._spent_time, status:data.status, data:data.results.S3});
 			},
 			6000
 		);	
