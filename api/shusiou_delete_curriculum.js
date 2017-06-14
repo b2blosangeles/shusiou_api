eb403df2cdb6fe8ce992599fb63b30117a520b1c
b2blosangeles <b2blosangeles@gmail.com>
2017-06-13 23:42:43 -0700

    Create shusiou_delete_curriculum.js

--- api/shusiou_delete_curriculum.js
+++ api/shusiou_delete_curriculum.js
@@ -1,16 +1,15 @@
 var mysql = require(env.space_path + '/api/inc/mysql/node_modules/mysql');
+var cfg0 = require(env.space_path + '/api/cfg/db.json');
+var connection = mysql.createConnection(cfg0);
+
 var CP = new pkg.crowdProcess();
 var _f = {};
 
 _f['S1'] = function(cbk) {
-	var cfg0 = require(env.space_path + '/api/cfg/db.json');
-	var connection = mysql.createConnection(cfg0);
-	connection.connect();
-
 	var str = 'DELETE FROM curriculums WHERE `id` = "' + req.body.cid + '"); ';
 	
 	connection.query(str, function (error, results, fields) {
-		connection.end();
+		
 		if (error) {
 			cbk(error.message);
 			return true;
@@ -25,10 +24,11 @@ _f['S1'] = function(cbk) {
 		}
 	});  
 };
-
+connection.connect();
 CP.serial(
 	_f,
 	function(data) {
+		connection.end();
 		res.send({_spent_time:data._spent_time, status:data.status, data:data});
 	},
 	30000
