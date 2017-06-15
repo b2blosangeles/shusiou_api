43e8564bca55063b81d1d54b027b6d749695593f
b2blosangeles <b2blosangeles@gmail.com>
2017-06-15 10:58:42 -0700

    Create shusiou_save_curriculum.js

---
+++ api/shusiou_save_curriculum.js
@@ -0,0 +1,42 @@
+var mysql = require(env.space_path + '/api/inc/mysql/node_modules/mysql');
+var CP = new pkg.crowdProcess();
+var _f = {};
+
+_f['S1'] = function(cbk) {
+	var cfg0 = require(env.space_path + '/api/cfg/db.json');
+	var connection = mysql.createConnection(cfg0);
+	connection.connect();
+
+	var str = 'INSERT INTO  curriculums (`uiv`,`vid`,`name`,`mother_lang`,`learning_lang`,`created`) VALUES (' +
+	'"' + req.body.uid + '",' +
+	'"' + req.body.vid + '",' +
+	'"' + req.body.name + '",' +
+	'"' + '' + '",' +
+	'"' + '' + '",' +
+	'NOW()' +	
+	'); ';
+	
+	connection.query(str, function (error, results, fields) {
+		connection.end();
+		if (error) {
+			cbk(error.message);
+			return true;
+		} else {
+			if (results[0]) {
+				cbk(results[0]);
+				CP.skip = true;
+			} else {
+				cbk(false);
+			}
+
+		}
+	});  
+};
+
+CP.serial(
+	_f,
+	function(data) {
+		res.send({_spent_time:data._spent_time, status:data.status, data:data});
+	},
+	30000
+);
