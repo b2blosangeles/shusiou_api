8885a716143b2c4b2fd6972b05fe1f5ba3f17f20
b2blosangeles <b2blosangeles@gmail.com>
2017-06-20 07:28:25 -0700

    Update shusiou_save_curriculum.js

--- api/shusiou_save_curriculum.js
+++ api/shusiou_save_curriculum.js
@@ -43,6 +43,36 @@ switch(req.body.cmd) {
 			30000
 		);
 		break;
+	case 'delete':
+		var _f = {};
+
+		_f['S1'] = function(cbk) {
+			var str = 'DELETE FROM  curriculums WHERE `id` ="' + req.body.cid + '" AND `uid` = "' + req.body.uid + '"); ';
+			connection.query(str, function (error, results, fields) {
+				if (error) {
+					cbk(error.message);
+					return true;
+				} else {
+					if (results[0]) {
+						cbk(results[0]);
+						CP.skip = true;
+					} else {
+						cbk(false);
+					}
+
+				}
+			});  
+		};
+		connection.connect();
+		CP.serial(
+			_f,
+			function(data) {
+				connection.end();
+				res.send({_spent_time:data._spent_time, status:data.status, data:data});
+			},
+			30000
+		);
+		break;		
 	default:
 		res.send({seatus:'error', message:'Missing or Wrong CMD!'});
 }		
