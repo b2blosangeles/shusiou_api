514f6950998617ff398a3d748a471ca23669f19a
b2blosangeles <b2blosangeles@gmail.com>
2017-06-20 15:26:29 -0700

    Update shusiou_get_curriculum_by_id.js

--- api/shusiou_get_curriculum_by_id.js
+++ api/shusiou_get_curriculum_by_id.js
@@ -8,26 +8,8 @@ var _f = {};
 _f['S1'] = function(cbk) {
 	var cfg0 = require(env.space_path + '/api/cfg/db.json');
 	var connection = mysql.createConnection(cfg0);
-	
 
-	var str = 'SELECT A.*, B.code FROM  `curriculums` A LEFT JOIN `videos` B ON A.vid = B.id; ';
-	
-	connection.query(str, function (error, results, fields) {
-		
-		if (error) {
-			cbk(error.message);
-			return true;
-		} else {
-			cbk(results);
-		}
-	});  
-};
-_f['S2'] = function(cbk) {
-	var cfg0 = require(env.space_path + '/api/cfg/db.json');
-	var connection = mysql.createConnection(cfg0);
-	
-
-	var str = 'SELECT A.*, B.code FROM  `curriculums` A LEFT JOIN `videos` B ON A.vid = B.id; ';
+	var str = 'SELECT A.*, B.code FROM  `curriculums` A LEFT JOIN `videos` B ON A.vid = B.id ABD A.id = "' + req.body.cid + '"; ';
 	
 	connection.query(str, function (error, results, fields) {
 		
