17345d90d2e2c920f1deee9324a4f6d60096fd07
b2blosangeles <b2blosangeles@gmail.com>
2017-06-20 15:51:47 -0700

    Update shusiou_get_curriculum_by_id.js

--- api/shusiou_get_curriculum_by_id.js
+++ api/shusiou_get_curriculum_by_id.js
@@ -24,12 +24,28 @@ _f['S1'] = function(cbk) {
 		}
 	});  
 };
+_f['S2'] = function(cbk) {
+	var cfg0 = require(env.space_path + '/api/cfg/db.json');
+	var connection = mysql.createConnection(cfg0);
+
+	var str = 'SELECT *  FROM  `videos` WHERE id = "' + CP.data.S1.vid + '"; ';
+	
+	connection.query(str, function (error, results, fields) {
+		
+		if (error) {
+			cbk(error.message);
+			return true;
+		} else {
+			cbk(results);
+		}
+	});  
+};
 connection.connect();
 CP.serial(
 	_f,
 	function(data) {
 		connection.end();
-		res.send({_spent_time:data._spent_time, status:data.status, data:data.results.S1});
+		res.send({_spent_time:data._spent_time, status:data.status, video:data.results.S1});
 	},
 	3000
 );
