c68586674c25650822567ed1ad3dc15e7dabae0c
b2blosangeles <b2blosangeles@gmail.com>
2017-06-21 14:14:32 -0700

    Update shusiou_curriculum.js

--- api/shusiou_curriculum.js
+++ api/shusiou_curriculum.js
@@ -142,7 +142,7 @@ switch(req.body.cmd) {
 			_f,
 			function(data) {
 				connection.end();
-				res.send({_spent_time:data._spent_time, status:data.status, data:data.results.S1});
+				res.send({_spent_time:data._spent_time, status:data.status, data:data.results.S1.insertId});
 			},
 			30000
 		);
