3ed733f995c18dfae305dd70cf3ae5f8642b9d00
b2blosangeles <b2blosangeles@gmail.com>
2017-06-27 13:49:47 -0700

    Update shusiou_curriculum.js

--- api/shusiou_curriculum.js
+++ api/shusiou_curriculum.js
@@ -11,6 +11,8 @@ switch(req.body.cmd) {
 				cbk({}); return true;
 			}
 			var str = 'SELECT A.*, B.`script` FROM  `curriculums` AS A LEFT JOIN `curriculum_sections`  AS B ON A.id = B.cid WHERE A.id = "' + req.body.cid + '"; ';
+			cbk(str);
+			return true;
 			connection.query(str, function (error, results, fields) {
 				if (error) {
 					cbk(error.message);
