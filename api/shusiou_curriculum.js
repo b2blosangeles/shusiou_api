8556a33ffd87fec61f7e353ee4213d33a24a12b2
b2blosangeles <b2blosangeles@gmail.com>
2017-06-27 14:09:16 -0700

    Update shusiou_curriculum.js

--- api/shusiou_curriculum.js
+++ api/shusiou_curriculum.js
@@ -16,7 +16,7 @@ switch(req.body.cmd) {
 					cbk(error.message);
 					return true;
 				} else {
-					results[0].o = results[0].script;
+				//	results[0].o = results[0].script;
 					//delete results[0].script;
 					cbk(results[0]);
 				}
