1c8ed803a79a5d20f9a3af9c4783b39673cfd756
b2blosangeles <b2blosangeles@gmail.com>
2017-07-08 07:40:32 -0700

    Update shusiou_queue_download.js

--- api/shusiou_queue_download.js
+++ api/shusiou_queue_download.js
@@ -6,7 +6,7 @@ var step = 30000000;
 
 var CP = new pkg.crowdProcess();
 var _f = {};
-
+/*
 var cfg0 = require(env.space_path + '/api/cfg/db.json');
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
@@ -34,7 +34,7 @@ var cfg0 = require(env.space_path + '/api/cfg/db.json');
 
 
 return true;
-
+*/
 _f['Q1'] = function(cbk) {
 	var cfg0 = require(env.space_path + '/api/cfg/db.json');
 	var connection = mysql.createConnection(cfg0);
