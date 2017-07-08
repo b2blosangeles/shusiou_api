56658f96fd56cbf1670478ad4991f2e8bfe98a31
b2blosangeles <b2blosangeles@gmail.com>
2017-07-08 07:31:07 -0700

    Update shusiou_queue_download.js

--- api/shusiou_queue_download.js
+++ api/shusiou_queue_download.js
@@ -7,7 +7,32 @@ var step = 30000000;
 var CP = new pkg.crowdProcess();
 var _f = {};
 
-res.send('niu');
+var cfg0 = require(env.space_path + '/api/cfg/db.json');
+	var connection = mysql.createConnection(cfg0);
+	connection.connect();
+
+	var str = 'SELECT * FROM  `video_queue` WHERE `source` = "youtube" AND `status` = 0 ORDER BY `created` ASC LIMIT 100; ';
+
+	connection.query(str, function (error, results, fields) {
+		connection.end();
+		if (error) {
+			cbk(error.message);
+			return true;
+		} else {
+			if (results.length) {
+				res.send(results[0]);
+			//	CP.skip = true;
+			} else {
+				res.send(false);
+			}
+
+		}
+	});  
+
+
+
+
+
 return true;
 
 _f['Q1'] = function(cbk) {
