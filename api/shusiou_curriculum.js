caf776e041769923174fbe9856653308272b388c
b2blosangeles <b2blosangeles@gmail.com>
2017-07-11 13:24:13 -0700

    Update shusiou_curriculum.js

--- api/shusiou_curriculum.js
+++ api/shusiou_curriculum.js
@@ -66,7 +66,7 @@ switch(req.body.cmd) {
 			var connection = mysql.createConnection(cfg0);
 
 
-			var str = 'SELECT A.*, B.code FROM  `curriculums` A LEFT JOIN `videos` B ON A.vid = B.id; ';
+			var str = 'SELECT A.*, B.code FROM  `curriculums` A LEFT JOIN `videos` B ON A.vid = B.id and A.uid = 7; ';
 
 			connection.query(str, function (error, results, fields) {
 
