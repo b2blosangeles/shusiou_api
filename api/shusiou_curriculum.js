e202fb8864fbce5c92cc3ee49b76a160dba62375
b2blosangeles <b2blosangeles@gmail.com>
2017-07-11 22:07:09 -0700

    Update shusiou_curriculum.js

--- api/shusiou_curriculum.js
+++ api/shusiou_curriculum.js
@@ -88,6 +88,35 @@ switch(req.body.cmd) {
 			3000
 		);
 		break;	
+	case 'getPublicList':
+		var _f = {};
+		_f['S1'] = function(cbk) {
+			var cfg0 = require(env.space_path + '/api/cfg/db.json');
+			var connection = mysql.createConnection(cfg0);
+
+
+			var str = 'SELECT A.*, B.code FROM  `videos` B JOIN `curriculums` A  ON A.vid = B.id AND A.publiched = 1;';
+
+			connection.query(str, function (error, results, fields) {
+
+				if (error) {
+					cbk(error.message);
+					return true;
+				} else {
+					cbk(results);
+				}
+			});  
+		};
+		connection.connect();
+		CP.serial(
+			_f,
+			function(data) {
+				connection.end();
+				res.send({_spent_time:data._spent_time, status:data.status, data:data.results.S1});
+			},
+			3000
+		);
+		break;			
 	case 'update':
 		var _f = {};
 
