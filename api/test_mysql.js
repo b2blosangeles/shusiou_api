bb6153a6bedc7147ab616f939332e423e0f9df51
b2blosangeles <b2blosangeles@gmail.com>
2017-07-13 11:46:36 -0700

    Update test_mysql.js

--- api/test_mysql.js
+++ api/test_mysql.js
@@ -1,2 +1,10 @@
 var v = req.body.ip;
 res.send(v[0]);
+pkg.request({
+    url: 'http://'+v[0]+'/checkip/',
+    method: "POST",
+    json: {}
+    }, function (error, resp, body) { 
+      res.send(body+'---');
+   });
+
