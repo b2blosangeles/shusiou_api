8e55b21d2eb987615b27c052c2edc0d005d4af5f
b2blosangeles <b2blosangeles@gmail.com>
2017-07-13 12:38:29 -0700

    Update test_mysql.js

--- api/test_mysql.js
+++ api/test_mysql.js
@@ -1,5 +1,5 @@
 var v = req.body.ip;
-
+/*
 function isIp(ip) {
     var arrIp = ip.split(".");
     if (arrIp.length !== 4) return "Invalid IP";
@@ -9,7 +9,7 @@ function isIp(ip) {
     }
     return true;
 }
-
+*/
 var CP = new pkg.crowdProcess();
 var _f = {};
 
