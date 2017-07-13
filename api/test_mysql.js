e02eddcbd666bdf0c11022fd88b5ba12f213b939
b2blosangeles <b2blosangeles@gmail.com>
2017-07-13 11:57:01 -0700

    Update test_mysql.js

--- api/test_mysql.js
+++ api/test_mysql.js
@@ -1,7 +1,20 @@
 var v = req.body.ip;
 
+function isIp(ip) {
+    var arrIp = ip.split(".");
+    if (arrIp.length !== 4) return "Invalid IP";
+    for (let oct of arrIp) {
+        if ( isNaN(oct) || Number(oct) < 0 || Number(oct) > 255)
+            return false;
+    }
+    return true;
+}
+
 pkg.request({
     url: 'http://'+v[0]+'/checkip/',
+    headers: {
+    "content-type": "application/json",
+    },    
     method: "GET"
     }, function (error, resp, body) { 
       res.send(typeof body);
