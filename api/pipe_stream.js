fc443d52c6d1681aecd611f7634c6693c2e2e213
b2blosangeles <b2blosangeles@gmail.com>
2017-07-16 06:39:04 -0700

    Update pipe_stream.js

--- api/pipe_stream.js
+++ api/pipe_stream.js
@@ -1,11 +1,10 @@
 var folder_base = '/mnt/shusiou-video/youtube/';
-var fs = require('fs');
-var url =  folder_base + req.param('video') + '/' + req.param('fn');
-fs.stat(url, function(err, data) {
+var fn =  folder_base + req.param('video') + '/' + req.param('fn');
+pke.fs.stat(fn, function(err, data) {
     if (err) 
       res.send('it does not exist');
     else {
-	var file = fs.createReadStream(fn);
+	var file = fs.createReadStream();
         file.pipe(res);
     }
   });
