633b17afe08af49ef0dbeafd50400244f5157d7c
b2blosangeles <b2blosangeles@gmail.com>
2017-07-16 06:34:35 -0700

    Update pipe_stream.js

--- api/pipe_stream.js
+++ api/pipe_stream.js
@@ -1,6 +1,8 @@
 var folder_base = '/mnt/shusiou-video/youtube/';
 var fs = require('fs');
 var url =  folder_base + req.param('video') + '/' + req.param('fn');
+res.send(url);
+return true;
 fs.stat(folder_base + req.param('video') + '/' + req.param('fn'), function(err, data) {
     if (err) 
       res.send('it does not exist');
