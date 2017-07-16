19fc2f7638051ac093efdb6b1585c6a87baf65cb
b2blosangeles <b2blosangeles@gmail.com>
2017-07-16 07:14:55 -0700

    Update filescan_video_list.js

--- api/filescan_video_list.js
+++ api/filescan_video_list.js
@@ -1,44 +1,4 @@
-var total_size = 0, _result = [], master_video = {}, mtime = '', last_file = '';
-
-function scan(dir, cbk) {
-    var d = dir || '.';
- 
-    var finder = require(env.space_path + '/api/inc/findit/findit.js')(d);
-    var path = require('path');
-
-    finder.on('directory', function (dir, stat, stop) {
-        var base = path.basename(dir);
-        total_size += stat.size;
-    });
-    finder.on('file', function (file, stat) {
-       var filter = /(\/tmp\_section\/)/;
-       var filter_master = /\/video\/video\.mp4$/;
-        var patt = new RegExp('^'+ dir);
-      
-       if (filter_master.test(file)) {
-          master_video = {folder:dir, master_video:file.replace(patt,''), mtime:stat.mtime, size:stat.size};
-       }  else if (!filter.test(file)) {
-           total_size += stat.size;
-           if (!mtime) mtime = stat.mtime;
-           if (new Date(stat.mtime) > new Date(mtime)) {
-               mtime = stat.mtime;
-               last_file = file.replace(patt,'');
-           }
-           _result[_result.length] = {path:file.replace(patt,''), mtime:stat.mtime, size:stat.size};
-       }
-    });
-    finder.on('link', function (link, stat) { });
-    
-    finder.on('end', function (link, stat) {
-        if (typeof cbk == 'function') {
-            cbk();
-        }
-    });
-
-
-}
-// '/mnt/shusiou-video/youtube/962SfJ00tYM/'
-scan('/mnt/shusiou-video/youtube/', function() {
-    master_video['totalsize'] = total_size;
-    res.send({master:master_video, laster_file:{file:last_file, mtime:mtime}, list:_result});
+pkg.fs.readdir('/mnt/shusiou-video/youtube/', function(err, files) {
+    if (err) res.send(err.message);
+    else res.send(files);
 });
