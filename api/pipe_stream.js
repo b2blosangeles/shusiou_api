707b41ed07ccfaebb0e982104b18a85255a11400
b2blosangeles <b2blosangeles@gmail.com>
2017-07-16 06:40:38 -0700

    Update pipe_stream.js

--- api/pipe_stream.js
+++ api/pipe_stream.js
@@ -3,7 +3,7 @@ pkg.fs.stat(fn, function(err, data) {
     if (err) 
       res.send('it does not exist');
     else {
-	var file = fs.createReadStream();
+	var file = fs.createReadStream(fn);
         file.pipe(res);
     }
   });
