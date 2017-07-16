ea0471758f2577ad53f665effcf3bdcdb9a31589
b2blosangeles <b2blosangeles@gmail.com>
2017-07-15 21:51:13 -0700

    Update pipe_stream.js

--- api/pipe_stream.js
+++ api/pipe_stream.js
@@ -2,7 +2,7 @@ var vid = req.param('vid');
 var folder_base = '/mnt/shusiou-video/youtube/';
 var fn =  '/mnt/shusiou-video/youtube/962SfJ00tYM/images/962SfJ00tYM/180_11.png';
 
-var fn =  '/mnt/shusiou-video/youtube/962SfJ00tYM/video/video.mp4';
+// var fn =  '/mnt/shusiou-video/youtube/962SfJ00tYM/video/video.mp4';
 var fs = require('fs');
 
 fs.stat(fn, function(err, data) {
