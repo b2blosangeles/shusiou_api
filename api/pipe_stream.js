b0e30c0de94372e528338de273772537f1a13213
b2blosangeles <b2blosangeles@gmail.com>
2017-07-17 22:38:55 -0700

    Update pipe_stream.js

--- api/pipe_stream.js
+++ api/pipe_stream.js
@@ -1,4 +1,4 @@
-var folder_base = '/mnt/shusiou-video/youtube/', fn =  folder_base + req.param('video') + '/' + req.param('fn');
+var folder_base = '/mnt/shusiou-video/youtube/', fn =  folder_base + req.param('fn');
 pkg.fs.stat(fn, function(err, data) {
     if (err) 
       res.send('it does not exist');
