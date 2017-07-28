c8aa9b111367f93d37b23d1c0bb352c94a16c775
b2blosangeles <b2blosangeles@gmail.com>
2017-07-28 13:04:16 -0700

    Update shusiou_get_video_nodes.js

--- api/shusiou_get_video_nodes.js
+++ api/shusiou_get_video_nodes.js
@@ -22,7 +22,7 @@ _f['Q0'] = function(cbk) {
 			var v = '', a=[];
 			if ((results) && (results.length)) {
 				a = results[0].nodes.split(',');
-				v = a[0];
+				v = s[Math.floor(Math.random()*s.length)];
 			} 
 			
 			cbk((!v)?'api.shusiou.com':v);	
