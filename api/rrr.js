03c2e5898170e0eca4f4682d835b57ac4a097d79
b2blosangeles <b2blosangeles@gmail.com>
2017-08-01 14:28:54 -0700

    Update rrr.js

--- api/rrr.js
+++ api/rrr.js
@@ -1,10 +1,12 @@
 var mysql = require(env.space_path + '/api/inc/mysql/node_modules/mysql');
+
+var CP = new pkg.crowdProcess();
+var _f = {};
+
 var cfg0 = require(env.space_path + '/api/cfg/db.json');
 var connection = mysql.createConnection(cfg0);
 connection.connect();
 
-var CP = new pkg.crowdProcess();
-var _f = {};
 _f['Q0'] = function(cbk) {
 	var str = 'DELETE FROM `cloud_node` WHERE  `status` > "2"; ';
 	connection.query(str, function (error, results, fields) {
@@ -17,73 +19,160 @@ _f['Q0'] = function(cbk) {
 	});  
 };
 
-_f['Q1'] = function(cbk) {
-	var str = 'SELECT * FROM  `cloud_node` WHERE  1; ';
+
+_f['S0'] = function(cbk) {
+	var str = 'SELECT * FROM `videos` WHERE 1;';
+	connection.query(str, function (error, results, fields) {	
+		if (error) {
+			cbk(error.message);
+			return true;
+		} else {
+			var v = {};
+			for (var i=0; i<results.length; i++) {
+				v[results[i].id] = Math.round(results[i].size/1000000);
+			}
+			cbk(v);
+		}
+	});  
+};
+_f['S1'] = function(cbk) {
+	var str = 'SELECT * FROM  `cloud_node` WHERE 1 ';
 	connection.query(str, function (error, results, fields) {
 		if (error) {
 			cbk(error.message);
-			CP.exit = 1;
+			return true;
+		} else {
+			var v = {};
+			for (var i=0; i<results.length; i++) {
+				v[results[i].node_ip] = results[i].total_space;
+			}
+			cbk(v);
+		}
+	});  
+};
+
+function sum(a, d) {
+	var v = 0;
+	for (var i=0; i < a.length; i++) {
+		v += d[a[i]];	
+	}
+	return v;
+}
+function cap(v, idx, traffic) {
+	var max = 2;
+	if ((traffic) && (traffic[idx])) max = traffic[idx];
+	if (v[idx].length >= max) {
+		return true;	
+	}
+}
+_f['Q0'] = function(cbk) {
+	var S = {}, C = {}, T = {}, short_a = [], short_b = 0;
+	
+	for (var o in CP.data.S0) {
+		var ex = false;
+		if (!C[o]) C[o] = [];
+		for (var p in CP.data.S1) {
+			if (C[o].length) {
+				ex = true;
+				break;
+			}	
+			if (!S[p]) S[p] = [];
+			if (sum(S[p], CP.data.S0) < (CP.data.S1[p] * 0.3)) {
+				S[p][S[p].length] = o;
+				C[o][C[o].length] = p;
+				ex = true;;
+				break;
+			}
+		}
+		if (!ex) short_a[short_a.length] = o;
+	}
+
+	for (var o in CP.data.S0) {
+		var ex = false;
+		if (!C[o]) C[o] = [];
+		
+		for (var p in CP.data.S1) {
+			if (!S[p]) S[p] = [];
+			
+			if (S[p].indexOf(o) == -1) {
+				if (sum(S[p], CP.data.S0) < (CP.data.S1[p] * 0.3)) {
+					if (cap(C,o, T)) {
+						var ex = true;
+						break;
+					} else {
+						S[p][S[p].length] = o;
+						C[o][C[o].length] = p;
+					}
+				}
+			}
+			
+		}
+		if (!ex) short_b++;
+	}
+	var CP1 = new pkg.crowdProcess();
+	var _f1 = {};
+	
+	for (var o in S) {
+		_f1[o] = (function(o) {
+			return function(cbk) {
+				var str = 'UPDATE `cloud_node` SET `list` =  "' + S[o].join(',') + '" WHERE `node_ip` = "' + o + '" ';
+				connection.query(str, function (error, results, fields) {
+					if (error) {
+						cbk(error.message);
+						return true;
+					} else {
+						cbk(S[o]);
+					}
+				});  
+			}	
+		})(o);
+	}	
+	for (var o in C) {
+		_f1['video_'+o] = (function(o) {
+			return function(cbk) {
+				var str = 'INSERT INTO `video_node` (`video_id`,`nodes`) VALUES ("' + o + '","' + C[o].join(',') + '") '+
+				'ON DUPLICATE KEY UPDATE `nodes` = "' +  C[o].join(',') + '"';
+
+				connection.query(str, function (error, results, fields) {
+					if (error) {
+						cbk(error.message);
+						return true;
+					} else {
+						cbk(C[o]);
+					}
+				});  
+			}	
+		})(o);	
+	}	
+	CP1.serial(
+		_f1,
+		function(data) {
+			// cbk({niu:'A001',S:S, C:C, short:{LV:short_a, L1:short_a.length,L2:short_b}});
+			cbk(data);
+		},
+		10000
+	);
+	
+};
+
+
+_f['S3'] = function(cbk) {
+	var str = 'SELECT * FROM  `video_node` WHERE 1 ';
+	connection.query(str, function (error, results, fields) {
+		if (error) {
+			cbk(error.message);
+			return true;
 		} else {
 			cbk(results);
 		}
 	});  
 };
 
-_f['Q2'] = function(cbk) {
-	var v = CP.data.Q1;
-	var CP1 = new pkg.crowdProcess();
-	var _f1 = {};	
-	for (var i=0; i <v.length; i++) {
-		_f1['S_'+i] = (function(i) {
-			return function(cbk1) {
-				var r = pkg.request({
-					url: 'http://'+v[i].node_ip+'/checkip/',
-					headers: {
-					    "content-type": "application/json"
-					},
-					timeout: 500
-				    }, function (error, resp, body) {
-					if (error) {			
-						var str = 'UPDATE `cloud_node` SET status = status + 1 WHERE  id = "' + v[i].id + '"; ';
-						connection.query(str, function (error, results, fields) {
-							if (error) {
-								cbk1(error.message);
-							} else {
-								cbk1(false);
-							}
-						}); 						
-					} else {								
-						if  (v[i].status) {
-							var str = 'UPDATE `cloud_node` SET status = 0 WHERE  id = "' + v[i].id + '"; ';
-							connection.query(str, function (error, results, fields) {
-								if (error) {
-									cbk1(error.message);
-								} else {
-									cbk1(true);
-								}
-							});							
-						} else {
-							cbk1(true);
-						}	
-					}
-				});	
-			}
-		})(i);
-	}
-	CP1.parallel(
-		_f1,
-		function(data) {
-			cbk(data);	
-		},
-		30000
-	);
-};
 CP.serial(
 	_f,
 	function(data) {
 		connection.end();
-		res.send(data);	
+		res.send(data.results);	
 	},
-	30000
+	10000
 );
-
