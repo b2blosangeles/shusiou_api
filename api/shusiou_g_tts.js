18033446516a943efb667830ac263e0157ee97be
b2blosangeles <b2blosangeles@gmail.com>
2017-08-11 11:30:53 -0700

    Update shusiou_g_tts.js

--- api/shusiou_g_tts.js
+++ api/shusiou_g_tts.js
@@ -11,12 +11,13 @@ if (!lang) {
 var sh = require(env.space_path + '/api/inc/shorthash/node_modules/shorthash');
 var fn = '/var/vv/vr/icona/'+sh.unique(str+'_'+lang)+'.mp3';
 
-res.send(fn);
+
 
  pkg.fs.stat(fn, function(err, data) {
      // if (err) {  
      if (true) { 	     
 	var googleTTS = require(env.space_path + '/api/lang_space/inc/google-tts-api/node_modules/google-tts-api/');
+	     res.send(fn);
 	googleTTS(str, lang, 1)   // speed normal = 1 (default), slow = 0.24 
 	.then(function (url) {
 	   var fs = require('fs');
