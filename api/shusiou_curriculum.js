6f425005bbb16f3cb7001f8af9fc68af07bd7635
b2blosangeles <b2blosangeles@gmail.com>
2017-08-13 07:11:46 -0700

    Update shusiou_curriculum.js

--- api/shusiou_curriculum.js
+++ api/shusiou_curriculum.js
@@ -17,7 +17,7 @@ switch(req.body.cmd) {
 					return true;
 				} else {
 					try {
-						var v = JSON.parse(decodeURIComponent(results[0].script));
+						var v = JSON.parse(decodeURIComponent(results[0].script0));
 						for (var i = 0; i < v.length; i++) {
 							if (v[i].track) {
 								v[i].track.s = parseFloat(v[i].track.s);
