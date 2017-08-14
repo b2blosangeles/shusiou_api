f698b9b278c4a132945fff7e382239d08ca98cbf
b2blosangeles <b2blosangeles@gmail.com>
2017-08-14 13:29:51 -0700

    Update curriculum_templates.js

--- api/curriculum_templates.js
+++ api/curriculum_templates.js
@@ -1,20 +1,20 @@
 var default0 = {
-					lang:{mother:'en-US', learning:'zh-CN'},
-					pro:[
-						{text:'The next sentence is saying|The next paragraph is talking about', lang:'en-US', active:true},
-						{text:'{l}', lang:'en-US', active:true},
-						{text:'Listen', lang:'en-US', active:true},
-						{action:'play video', active:true}, 
-						{text:'Please repeat', lang:'en-US', active:false}
-					],					
-					match:[
-						{text:'Good Job|Nice|good|excellent', lang:'en-US', active:true},
-					],
-					nomatch:[
-						{text:'Incorrect, please try again|Wrong, repeat', lang:'en-US', active:false},
-						{action:'play video', active:true}, 
-						{text:'Please try again.', lang:'en-US', active:false}					
-					],
-					done:{text:'Lets continue', lang:'en-US', active:true}
-				};
+	lang:{mother:'en-US', learning:'zh-CN'},
+	pro:[
+		{text:'The next sentence is saying|The next paragraph is talking about', lang:'en-US'},
+		{text:'{l}', lang:'en-US'},
+		{text:'Listen', lang:'en-US'},
+		{action:'play video'}, 
+		{text:'Please repeat', lang:'en-US'}
+	],					
+	match:[
+		{text:'Good Job|Nice|good|excellent', lang:'en-US'},
+	],
+	nomatch:[
+		{text:'Incorrect, please try again|Wrong, repeat', lang:'en-US'},
+		{action:'play video'}, 
+		{text:'Please try again.', lang:'en-US'}					
+	],
+	done:{text:'Lets continue', lang:'en-US'}
+};
 res.send(default0);
