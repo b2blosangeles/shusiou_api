5e8a5cb9cd6bfd88ae82ca29586e1d9e787dd5d2
b2blosangeles <b2blosangeles@gmail.com>
2017-08-14 09:23:17 -0700

    Update and rename pipe_stream.js to curriculum_templates.js

--- api/pipe_stream.js
+++
@@ -1,9 +0,0 @@
-var folder_base = '/mnt/shusiou-video/youtube/', fn =  folder_base + req.param('fn');
-pkg.fs.stat(fn, function(err, data) {
-    if (err) 
-      res.send('it does not exist');
-    else {
-	var file = pkg.fs.createReadStream(fn);
-        file.pipe(res);
-    }
-  });
