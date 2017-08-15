b19dd5a5326c9e3fab35a3e2144be509168f4cc7
b2blosangeles <b2blosangeles@gmail.com>
2017-08-14 17:19:01 -0700

    Update curriculum_templates.js

--- api/curriculum_templates.js
+++ api/curriculum_templates.js
@@ -1,2 +1,20 @@
-var _default = {};
-res.send('default');
+var _default = {
+  lang:{mother:'en-US', learning:'zh-CN'},
+	pro:[
+		{text:'The next sentence is saying|The next paragraph is talking about', lang:'en-US'},
+		{text:'{l}', lang:'en-US'},
+		{text:'Listen', lang:'en-US'},
+		{action:'play video'}, 
+		{text:'Please repeat', lang:'en-US'}
+	],					
+	match:[
+		{text:'Good Job|Nice|good|excellent', lang:'en-US'}
+	],
+	nomatch:[
+		{text:'Incorrect, please try again|Wrong, repeat', lang:'en-US'},
+		{action:'play video'}, 
+		{text:'Please try again.', lang:'en-US'}					
+	],
+	done:{text:'Lets continue', lang:'en-US'}
+};
+res.send(_default);
