e756352fe8ef0c2d666a7a3a2a22eb946da11e7d
b2blosangeles <b2blosangeles@gmail.com>
2017-08-17 13:54:48 -0700

    Update curriculum_templates.js

--- api/curriculum_templates.js
+++ api/curriculum_templates.js
@@ -2,7 +2,7 @@ var CP = new pkg.crowdProcess();
 var _f = {};
 		
 pkg.fs.readdir(env.space_path + '/api/templates/', function(err, files) {
-     for (var i in files)
+     for (var i in files) {
      _f[files[i]] = (function(i) {
 		return function(cbk) {
 			pkg.fs.readFile(env.space_path + '/api/templates/'+files[i], 'utf8', function(err, data) { 
