83d67668ac8a14bf8b0595ab1e0c4803253f3ea0
b2blosangeles <b2blosangeles@gmail.com>
2017-08-17 13:51:38 -0700

    Update curriculum_templates.js

--- api/curriculum_templates.js
+++ api/curriculum_templates.js
@@ -3,7 +3,7 @@ var _f = {};
 		
 pkg.fs.readdir(env.space_path + '/api/templates/', function(err, files) {
      for (var i in files)
-     _f[files[i]] = function(i) {
+     _f[files[i]] = (function(i) {
 	return function(cbk) {
 		pkg.fs.readFile(env.space_path + '/api/templates/'+files[i], 'utf8', function(err, data) { 
 		    try {
@@ -12,7 +12,7 @@ pkg.fs.readdir(env.space_path + '/api/templates/', function(err, files) {
 			cbk(false);
 		    }
 		 });
-	}	
+	})(i);	
      }
      CP.serial(
           _f,
