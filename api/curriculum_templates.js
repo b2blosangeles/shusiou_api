1b42f363a6336dec68590807dc06a02226717d32
b2blosangeles <b2blosangeles@gmail.com>
2017-08-17 15:11:02 -0700

    Update curriculum_templates.js

--- api/curriculum_templates.js
+++ api/curriculum_templates.js
@@ -2,41 +2,36 @@ var CP = new pkg.crowdProcess();
 var _f = {};
 		
 var mother = req.body.nother_languge, learning = req.body.nother_learnging;
-var v = []; 
+
+pkg.fs.readdir(env.space_path + '/api/templates/', function(err, files) {
+     for (var i in files) {
+	     _f[files[i]] = (function(i) {
+			return function(cbk) {
+				pkg.fs.readFile(env.space_path + '/api/templates/'+files[i], 'utf8', function(err, data) { 
+				    try {
+					cbk(JSON.parse(data)); 
+				    } catch (err) {
+					cbk(false);
+				    }
+				 });
+			}	
+		})(i);	
+     }
+     CP.serial(
+	  _f,
+	  function(data) {
+		var v = []; 
+		for (var i in files) {  
+			v[v.length] = data.results[files[i]];
+
+		}
+		res.send(v);
+	  },
+	  3000
+     );     
+
+});
 
 
-function getData() {
-
-	pkg.fs.readdir(env.space_path + '/api/templates/', function(err, files) {
-	     for (var i in files) {
-		     _f[files[i]] = (function(i) {
-				return function(cbk) {
-					pkg.fs.readFile(env.space_path + '/api/templates/'+files[i], 'utf8', function(err, data) { 
-					    try {
-						cbk(JSON.parse(data)); 
-					    } catch (err) {
-						cbk(false);
-					    }
-					 });
-				}	
-			})(i);	
-	     }
-	     CP.serial(
-		  _f,
-		  function(data) {
-
-			for (var i in files) {  
-				v[v.length] = data.results[files[i]];
-
-			}
-
-		  },
-		  3000
-	     );     
-
-	});
-}
-getData();
-res.send(v);
 
 
